69c295e95233c2b3828e5d7bc590267cc94c67da
Emirhan Cetinkaya <emirhan551952@gmail.com>
2025-01-26 12:27:34 +0300

    Enhance YouTube ad skipping and improve content script logic

--- content.js
+++ content.js
@@ -1,15 +1,24 @@
 function seekForward(video, seconds) {
   if (video && video.duration) {
-    video.currentTime = Math.min(video.currentTime + seconds, video.duration);
+    // Netflix için özel durum kontrolü
+    if (window.location.hostname === "www.youtube.com") {
+      video.currentTime = Math.min(video.currentTime + seconds, video.duration);
+    } else {
+      // Diğer siteler için varsayılan davranış
+      video.currentTime += seconds;
+    }
   }
 }
 
 function forwardVideos() {
-  const adButton = document.querySelector(".ytp-ad-button-icon");
-  if (adButton) {
+  console.log("Reklam kontrol ediliyor..."); // Her saniye kontrol edildiğini görmek için
+  // Reklam elementini daha spesifik bir seçici ile ara
+  const adButtonIcon = document.querySelector("span.ytp-ad-button-icon");
+  if (adButtonIcon) {
+    console.log("Reklam bulundu, video ileri sarılıyor."); // Konsola çıktı ver
     const mediaTags = Array.from(document.querySelectorAll("video"));
     mediaTags.forEach((video) => {
-      seekForward(video, 60);
+      seekForward(video, 60); // Reklam bulunduğunda 60 saniye ileri al
     });
   }
 }
@@ -19,14 +28,13 @@ if (window.location.href.startsWith("https://www.youtube.com/watch?v")) {
     const observer = new MutationObserver(function (mutationsList) {
       for (let mutation of mutationsList) {
         if (mutation.type === "childList") {
-          const adButton = document.querySelector(".ytp-ad-button-icon");
-          if (adButton) {
-            forwardVideos();
-          }
+          forwardVideos();
         }
       }
     });
     observer.observe(document.body, { childList: true, subtree: true });
-    setInterval(forwardVideos, 1000);
   });
+
+  // Her saniye forwardVideos fonksiyonunu çalıştır
+  setInterval(forwardVideos, 1000);
 }
